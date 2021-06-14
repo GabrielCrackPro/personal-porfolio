8ceff690f3ce67ee5ee0fe23559e4a66c3926665
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-06-14 20:56:41 +0200

    Project Started 🎉

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const public = express.static('public')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(morgan('dev'))
+app.use(public)
+
+app.listen(port, () => { `🚀Listening on port ${port}...` })
